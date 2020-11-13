0598dc5ec01f4bf1fe643cac95528095e60fca49
hojumoney <hojun0090@naver.com>
2020-11-13 19:35:04 +0900

    201113
    카카오태그 영역만 테스트로 태그 끌어왔는데 되는것같음. 근데 끌어온태그가 append 되고 삭제가 안되는게 문제

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑
 db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
 
 
-@app.route('/')
+@app.route('/') 
 def home():
     return render_template('index.html')
 
 #
-@app.route('/check', methods=['POST'])
-def check_url():
+@app.route('/show_tags', methods=['POST'])
+def show_tags():
     url_receive = request.form['url_give']
     url_result = tag_reader.check_url(url_receive)
     # 3. mongoDB에 데이터 넣기
@@ -27,17 +27,8 @@ def check_url():
     # db.articles.insert_one(doc)
     #
 
-    return jsonify({'result': 'success', 'msg' : url_receive + '태그 확인 되었습니다','data': url_result})
-#
-#
-# @app.route('/memo', methods=['GET'])
-# def read_articles():
-#     # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기(Read)
-#     articles = list(db.articles.find({},{'_id':False}))
-#     print(articles)
-#     # 2. articles라는 키 값으로 articles 정보 보내주기
-#     return jsonify({'result': 'success', 'data': articles})
+    return jsonify({'result': 'success', 'msg' : url_receive + '의 태그가 확인되었습니다','data': url_result})
 #
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
